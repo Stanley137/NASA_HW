a8375a35b17a68d4d3114ef242dd931c3a097678
Stanley137 <stanley2friend@gmail.com>
2024-03-02 15:45:49 +0800

    upload HW

--- Lab1_debugger/debugger.sh
+++ Lab1_debugger/debugger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # echo $1 $2 $3 $4
 #set -x # debugging
 # argument
@@ -6,27 +7,40 @@ arg_gen=$1
 ac_code=$2
 wa_code=$3
 times=$4
-# compile
+# compile and create file
+touch in.txt
+touch a.txt
+touch b.txt
 gcc $arg_gen -o gen
 gcc $ac_code -o a
 gcc $wa_code -o b
+
+# permission
+chmod +x gen
+chmod +x a
+chmod +x b
+chmod +rwx a.txt
+chmod +rwx b.txt
+chmod +rwx in.txt
+
 # test code
-for (( i = 1; i < $times; i++ )); do
+for (( i = 1; i <= $times; i++ )); do
 	./gen $i > in.txt
-	ac_result=`cat ./in.txt | ./a` 
-	wa_result=`cat ./in.txt | ./b`
-	if [[ "$ac_result" != "$wa_result"  ]]; then
+	cat ./in.txt | ./a > a.txt
+       	cat ./in.txt | ./b > b.txt
+	cmp -s a.txt b.txt
+	if [[ $? -ne 0 ]]; then
 		echo "Test $i"
 		echo "Input:"
 		cat ./in.txt
 		echo "--------------------"
 		echo "Output of $ac_code"
 		echo "--------------------"
-		echo $ac_result | awk '{print $1"\n"$2}'
+		cat ./a.txt
 		echo "--------------------"
 		echo "Output of $wa_code"
-		echo "--------------------"
-		echo $wa_result | awk '{print $1"\n"$2}'
+		echo "-------------------"
+		cat ./b.txt
 		echo "--------------------"
 	fi
 done
